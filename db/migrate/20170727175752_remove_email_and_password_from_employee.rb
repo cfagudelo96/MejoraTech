108087bacb2a113c4595ba0d8964de1763dcb883
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2017-08-01 14:36:59 -0500

    Corrección a heroku

--- db/migrate/20170727175752_remove_email_and_password_from_employee.rb
+++ db/migrate/20170727175752_remove_email_and_password_from_employee.rb
@@ -1,7 +1,6 @@
 class RemoveEmailAndPasswordFromEmployee < ActiveRecord::Migration[5.1]
   def change
     remove_column :employees, :email
-    remove_index :employees, :email
     remove_column :employees, :password
   end
 end
