6fae6c24ee7568c4b4a9d7ed33b295186243bf05
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-01-26 15:22:37 -0500

    W

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['MEJORATECH_AwS_ACCESS_KEY'],
+    aws_access_key_id: ENV['MEJORATECH_AWS_ACCESS_KEY'],
     aws_secret_access_key: ENV['MEJORATECH_AWS_SECRET_KEY'],
     region: ENV['MEJORATECH_AWS_S3_REGION'],
   }
